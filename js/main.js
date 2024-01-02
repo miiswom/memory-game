01618b0766e00e65f8fd1772682c34a7c3265c52
Farmata Kane <kane.farmata@gmail.com>
2024-01-02 19:03:04 +0100

    Use a loop to create grids

--- js/main.js
+++ js/main.js
@@ -13,67 +13,67 @@ const startButton = document.getElementById("start-button");
 
 const container = document.getElementById("container");
 // grab the div
-const blackDiv = document.getElementById("black-div1")/*.addEventListener("click", userClicks)*/;
-const brownDiv = document.getElementById("brown-div1")
-const fuschiaDiv = document.getElementById("fuchsia-div1");
-const lightblueDiv = document.getElementById("lightblue-div1");
-const limeDiv = document.getElementById("lime-div1");
-const orangeDiv = document.getElementById("orange-div1");
-const salmonDiv = document.getElementById("salmon-div1");
-const whiteDiv = document.getElementById("white-div1");
-const blackDivBis = document.getElementById("black-div2");
-const brownDivBis = document.getElementById("brown-div2");
-const fuschiaDivBis = document.getElementById("fuchsia-div2");
-const lightblueDivBis = document.getElementById("lightblue-div2");
-const limeDivBis = document.getElementById("lime-div2");
-const orangeDivBis = document.getElementById("orange-div2");
-const salmonDivBis = document.getElementById("salmon-div2");
-const whiteDivBis = document.getElementById("white-div2");
+// const blackDiv = document.getElementById("black-div1")/*.addEventListener("click", userClicks)*/;
+// const brownDiv = document.getElementById("brown-div1")
+// const fuschiaDiv = document.getElementById("fuchsia-div1");
+// const lightblueDiv = document.getElementById("lightblue-div1");
+// const limeDiv = document.getElementById("lime-div1");
+// const orangeDiv = document.getElementById("orange-div1");
+// const salmonDiv = document.getElementById("salmon-div1");
+// const whiteDiv = document.getElementById("white-div1");
+// const blackDivBis = document.getElementById("black-div2");
+// const brownDivBis = document.getElementById("brown-div2");
+// const fuschiaDivBis = document.getElementById("fuchsia-div2");
+// const lightblueDivBis = document.getElementById("lightblue-div2");
+// const limeDivBis = document.getElementById("lime-div2");
+// const orangeDivBis = document.getElementById("orange-div2");
+// const salmonDivBis = document.getElementById("salmon-div2");
+// const whiteDivBis = document.getElementById("white-div2");
 
-const divs = [blackDiv, brownDiv, fuschiaDiv, lightblueDiv, limeDiv, orangeDiv, salmonDiv, whiteDiv, 
-  blackDivBis, brownDivBis, fuschiaDivBis, lightblueDivBis, limeDivBis, orangeDivBis, salmonDivBis, whiteDivBis];
+// const divs = [blackDiv, brownDiv, fuschiaDiv, lightblueDiv, limeDiv, orangeDiv, salmonDiv, whiteDiv, 
+//   blackDivBis, brownDivBis, fuschiaDivBis, lightblueDivBis, limeDivBis, orangeDivBis, salmonDivBis, whiteDivBis];
 
-divs.forEach((div) => div.addEventListener("click", userClicks))
+// divs.forEach((div) => div.addEventListener("click", userClicks))
 
 // grab each image
-const blackImg = document.getElementById("black-img1");
-const brownImg = document.getElementById("brown-img1");
-const fuschiaImg = document.getElementById("fuchsia-img1");
-const lightblueImg = document.getElementById("lightblue-img1");
-const limeImg = document.getElementById("lime-img1");
-const orangeImg = document.getElementById("orange-img1");
-const salmonImg = document.getElementById("salmon-img1");
-const whiteImg = document.getElementById("white-img1");
-const blackImgBis = document.getElementById("black-img2");
-const brownImgBis = document.getElementById("brown-img2");
-const fuschiaImgBis = document.getElementById("fuchsia-img2");
-const lightblueImgBis = document.getElementById("lightblue-img2");
-const limeImgBis = document.getElementById("lime-img2");
-const orangeBis = document.getElementById("orange-img2");
-const salmonImgBis = document.getElementById("salmon-img2");
-const whiteImgBis = document.getElementById("white-img2");
+// const blackImg = document.getElementById("black-img1");
+// const brownImg = document.getElementById("brown-img1");
+// const fuschiaImg = document.getElementById("fuchsia-img1");
+// const lightblueImg = document.getElementById("lightblue-img1");
+// const limeImg = document.getElementById("lime-img1");
+// const orangeImg = document.getElementById("orange-img1");
+// const salmonImg = document.getElementById("salmon-img1");
+// const whiteImg = document.getElementById("white-img1");
+// const blackImgBis = document.getElementById("black-img2");
+// const brownImgBis = document.getElementById("brown-img2");
+// const fuschiaImgBis = document.getElementById("fuchsia-img2");
+// const lightblueImgBis = document.getElementById("lightblue-img2");
+// const limeImgBis = document.getElementById("lime-img2");
+// const orangeBis = document.getElementById("orange-img2");
+// const salmonImgBis = document.getElementById("salmon-img2");
+// const whiteImgBis = document.getElementById("white-img2");
 
-const allDivs = Array.prototype.slice.call(divs); 
-const randomAllDivs = allDivs[Math.floor(Math.random() * allDivs.length)];
+// const allDivs = Array.prototype.slice.call(divs); 
+// const randomAllDivs = allDivs[Math.floor(Math.random() * allDivs.length)];
 
 startButton.addEventListener("click", () => {
   container.classList.remove("hidden");
   startButton.classList.add("hidden");
-
-
   
-  // allDivs.forEach((div) => {
-  //   div.outerHTML;
-  //   if(div.outerHTML === randomAllDivs.outerHTML) {
-  //     return;
-  //   } else {
-  //     container.appendChild(div.outerHTML);
-  //   }
-  // })
+//  create the grid with a for loop
+// const divTest = document.getElementsByClassName('gallerycard');
+// container.appendChild(divTest);
+for(i =1; i < 17; i++) {
+  let div = document.createElement('div');
+  container.appendChild(div);
+  div.classList.add("gallerycard");
+  div.setAttribute("id", "black")
+}
+// remove the grids with a while loop
 })
 
 // useful arrays to reuse in function:
-const colorsImg = [blackImg, blackImgBis, brownImg, brownImgBis, fuschiaImg, fuschiaImgBis, lightblueImg, lightblueImgBis, limeImg, limeImgBis, orangeImg, orangeBis, salmonImg, salmonImgBis, whiteImg, whiteImgBis];
+// const colorsImg = [blackImg, blackImgBis, brownImg, brownImgBis, fuschiaImg, fuschiaImgBis, lightblueImg, lightblueImgBis, limeImg, limeImgBis, orangeImg, orangeBis, salmonImg, salmonImgBis, whiteImg, whiteImgBis];
 const colors = ["black", "brown", "fuchsia", "lightblue", "lime", "orange", "salmon", "white"];
 // scoring
 let match = 0;
