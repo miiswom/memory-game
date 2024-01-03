bcba081b403594bf5296061b9d0cd45f252cd5b0
Farmata Kane <kane.farmata@gmail.com>
2024-01-03 03:12:20 +0100

    Randomly places divs without userClicks()

--- js/main.js
+++ js/main.js
@@ -9,52 +9,16 @@
   
 const startButton = document.getElementById("start-button");
 
-
-
 const container = document.getElementById("container");
-// grab the div
-// const blackDiv = document.getElementById("black-div1")/*.addEventListener("click", userClicks)*/;
-// const brownDiv = document.getElementById("brown-div1")
-// const fuschiaDiv = document.getElementById("fuchsia-div1");
-// const lightblueDiv = document.getElementById("lightblue-div1");
-// const limeDiv = document.getElementById("lime-div1");
-// const orangeDiv = document.getElementById("orange-div1");
-// const salmonDiv = document.getElementById("salmon-div1");
-// const whiteDiv = document.getElementById("white-div1");
-// const blackDivBis = document.getElementById("black-div2");
-// const brownDivBis = document.getElementById("brown-div2");
-// const fuschiaDivBis = document.getElementById("fuchsia-div2");
-// const lightblueDivBis = document.getElementById("lightblue-div2");
-// const limeDivBis = document.getElementById("lime-div2");
-// const orangeDivBis = document.getElementById("orange-div2");
-// const salmonDivBis = document.getElementById("salmon-div2");
-// const whiteDivBis = document.getElementById("white-div2");
 
-// const divs = [blackDiv, brownDiv, fuschiaDiv, lightblueDiv, limeDiv, orangeDiv, salmonDiv, whiteDiv, 
-//   blackDivBis, brownDivBis, fuschiaDivBis, lightblueDivBis, limeDivBis, orangeDivBis, salmonDivBis, whiteDivBis];
+const divs = ["black-div1", "brown-div1", "fuchsia-div1", "lightblue-div1", "lime-div1", "orange-div1","salmon-div1", "white-div1", 
+              "black-div2", "brown-div2", "fuchsia-div2", "lightblue-div2", "lime-div2", "orange-div2","salmon-div2", "white-div2"];
 
-// divs.forEach((div) => div.addEventListener("click", userClicks))
+const divsBis = ["black-div1", "brown-div1", "fuchsia-div1", "lightblue-div1", "lime-div1", "orange-div1","salmon-div1", "white-div1", 
+              "black-div2", "brown-div2", "fuchsia-div2", "lightblue-div2", "lime-div2", "orange-div2","salmon-div2", "white-div2"];
 
 // grab each image
-// const blackImg = document.getElementById("black-img1");
-// const brownImg = document.getElementById("brown-img1");
-// const fuschiaImg = document.getElementById("fuchsia-img1");
-// const lightblueImg = document.getElementById("lightblue-img1");
-// const limeImg = document.getElementById("lime-img1");
-// const orangeImg = document.getElementById("orange-img1");
-// const salmonImg = document.getElementById("salmon-img1");
-// const whiteImg = document.getElementById("white-img1");
-// const blackImgBis = document.getElementById("black-img2");
-// const brownImgBis = document.getElementById("brown-img2");
-// const fuschiaImgBis = document.getElementById("fuchsia-img2");
-// const lightblueImgBis = document.getElementById("lightblue-img2");
-// const limeImgBis = document.getElementById("lime-img2");
-// const orangeBis = document.getElementById("orange-img2");
-// const salmonImgBis = document.getElementById("salmon-img2");
-// const whiteImgBis = document.getElementById("white-img2");
 
-// const allDivs = Array.prototype.slice.call(divs); 
-// const randomAllDivs = allDivs[Math.floor(Math.random() * allDivs.length)];
 
 startButton.addEventListener("click", () => {
   container.classList.remove("hidden");
@@ -66,14 +30,55 @@ startButton.addEventListener("click", () => {
 for(i =1; i < 17; i++) {
   let div = document.createElement('div');
   container.appendChild(div);
+  div.addEventListener("click", userClicks);
+
   div.classList.add("gallerycard");
-  div.setAttribute("id", "black")
+
+  let img = document.createElement('img');
+  div.appendChild(img);
+  img.classList.add("img", "hidden", "not-found");
+  
+  div.setAttribute("id", randomId());
+  function randomId() {
+    const nodeList = document.querySelectorAll(".gallerycard");
+    const array1 = Array.prototype.slice.call(nodeList);
+    let id = divs[Math.floor(Math.random() * divs.length)];
+    for (let i = divs.length; i > 0; i--) {
+      // if(usedId[i] == id) {
+      //   console.log("color match");;
+      // } 
+      if(!divs.includes(id)){
+        console.log("doesn't include");
+      } else {
+        img.setAttribute("src", `css/assets/${id}.svg`);
+        let newIdArr = id.split("")
+        let newIdArrSpliced = newIdArr.splice(-4, 3, 'img')
+        let newIdStr = newIdArr.join("");
+        colorsImg.push(`<img id="${newIdStr}" class="img" src="css/assets/${newIdStr}.svg">`);
+        const colorsImgArr = newIdArr.push()
+        divs.splice(divs.indexOf(id), 1);
+        console.log("includes");
+      }
+  } 
+    return id;
+  };
+  
+  // img.setAttribute("id", promptImgId())
 }
 // remove the grids with a while loop
 })
 
+
+
+// function promptImgSrc(node) {
+//   if(node.parentElement.outerHTML.contains(colors)) {
+
+//     return `src="css/assets/${colors}.svg"`
+//   }
+// }
+
 // useful arrays to reuse in function:
-// const colorsImg = [blackImg, blackImgBis, brownImg, brownImgBis, fuschiaImg, fuschiaImgBis, lightblueImg, lightblueImgBis, limeImg, limeImgBis, orangeImg, orangeBis, salmonImg, salmonImgBis, whiteImg, whiteImgBis];
+const colorsImg = [];
 const colors = ["black", "brown", "fuchsia", "lightblue", "lime", "orange", "salmon", "white"];
 // scoring
 let match = 0;
@@ -82,81 +87,125 @@ let noMatch = 0;
 let yesCount = 0;
 let noCount = 0;
 // create a function that runs when the user clicks a div
-function userClicks(e, child, a, b) {
+function userClicks(e, a, b) {
   document.body.style.background = colors[Math.floor(Math.random() * colors.length)];
   if (e.target.children[0].classList.contains("found")) {
     return;
   } else {
-  child = e.target.children[0];
+  // child = e.target.children[0];
   e.target.children[0].classList.remove("hidden");
   e.target.children[0].classList.add("showing");
   const nodeList = document.querySelectorAll('.showing'); 
-// console.log(nodeList); 
+// // console.log(nodeList); 
 
-// Converting using Array.prototype.slice.call 
-const array1 = Array.prototype.slice.call(nodeList); 
-if(array1.length >= 2) {
-  a = array1[0]/*.outerHTML*/;
-  b = array1[1]/*.outerHTML*/;
-  for(let i=0; i < colors.length; i++) {
-    if (a/*.includes*/ == colorsImg[i] || b/*.includes*/ == colorsImg[i]) {
-      console.log("Match");
-      match++;
-    } else {
-      console.log("No Match")
-      noMatch++
-    }
-  }
-if (match <= 2) {
-  colors.forEach( (color) => {
-    if(a.outerHTML.includes(color) && b.outerHTML.includes(color)) {
-    console.log("YES");
-    yesCount++;
-    match = 0;
-    noMatch = 0;
-    return;
-    } else { 
-    console.log("NO");
-    noCount++;
-    match = 0;
-    noMatch = 0;
-  }} )
-  // for(let i = 0; i < colors.length; i++) {
-  //   if(a.outerHTML.includes(colors[i]) === true && b.outerHTML.includes(colors[i])) {
-  //       a.classList.replace("showing", "found");
-  //       b.classList.replace("showing", "found");
-  //         match = 0;
+// // Converting using Array.prototype.slice.call 
+// const array1 = Array.prototype.slice.call(nodeList); 
+// console.log(array1);
+// if(array1.length >= 2) {
+//   a = array1[0];
+//   let strA = a.parentNode.id.toString();
+//   let nodeA = a.parentNode.children[0]
+
+//   b = array1[1];
+//   let strB = b.parentNode.id.toString();
+//   let nodeB = b.parentNode.children[0]
+
+//   console.log(nodeA, nodeB)
+
+//   for(let i=0; i < divsBis.length; i++) {
+//     if (strA/*.includes*/ == divsBis[i] || strB/*.includes*/ == divsBis[i]) {
+//       console.log("Match");
+//       match++;
+//     } else {
+//       console.log("No Match")
+//       noMatch++
+//     }
+//   }
+// if (match <= 2) {
+//   divsBis.forEach( (div) => {
+//     if(strA.includes(div) && strB.includes(div)) {
+//     console.log("YES");
+//     yesCount++;
+//     match = 0;
+//     noMatch = 0;
+//     return;
+//     } else { 
+//     console.log("NO");
+//     noCount++;
+//     match = 0;
+//     noMatch = 0;
+//   }} )
+//   for(let i = 0; i < colors.length; i++) {
+//     let newArrA = strA.split("");
+//     newArrA.splice(-5, 5);
+//     let newStrA = newArrA.join("");
+
+//     let newArrB = strB.split("");
+//     newArrB.splice(-5, 5);
+//     let newStrB = newArrB.join("");
+
+//     console.log(newArrA, newStrB)
+//     if(newStrA == colors[i] === true && newStrB == colors[i]) {
+//       // nodeA.classList.remove("showing");
+//       // nodeA.classList.add("hidden");
+//       // nodeB.classList.remove("showing");
+//       // nodeB.classList.add("hidden"); 
+//       console.log(nodeA, nodeB);
+//         nodeA.classList.replace("showing", "found");
+//         nodeB.classList.replace("showing", "found");
+//           match = 0;
           
-  //   } else {
-  //     a.classList.replace("showing", "hidden");
-  //     b.classList.replace("showing", "hidden");
-  //         match = 0;
-  //         return;
-  //   }}
-  };
-setTimeout ( () => {
-  if (yesCount == 1) {
-         a.classList.replace("showing", "found");
-         b.classList.replace("showing", "found");  
-         yesCount = 0;
-         noCount = 0;
-} else if (noCount >= 7) {
-       a.classList.replace("showing", "hidden");
-       b.classList.replace("showing", "hidden");
-       yesCount = 0;
-       noCount = 0;
-} else {
-  yesCount = 0;
-         noCount = 0;
-  return;
-}}, 500)
-} else {
-  console.log("ERROR")
-}
-}}
+//     } else {
+//       // nodeA.classList.remove("showing");
+//       // nodeA.classList.add("hidden");
+//       // nodeB.classList.remove("showing");
+//       // nodeB.classList.add("hidden"); 
+//       console.log(nodeA, nodeB);   
+//       nodeA.classList.replace("showing", "hidden");
+//       nodeB.classList.replace("showing", "hidden");
+//           match = 0;
+//           return;
+//     }}
+//   };
+// setTimeout ( () => {
+//   if (yesCount == 1) {
+//     // nodeA.classList.remove("showing");
+//     // nodeA.classList.add("found");
+//     // nodeB.classList.remove("showing");
+//     // nodeB.classList.add("found");#
+//     console.log(nodeA, nodeB)
+//     nodeA.classList.replace("showing", "hidden");
+//     nodeB.classList.replace("showing", "hidden");
+//          yesCount = 0;
+//          noCount = 0;
+// } else if (noCount >= 7) {
+//   console.log(nodeA, nodeB)
+//   // nodeA.classList.remove("showing");
+//   // nodeA.classList.add("hidden");
+//   // nodeB.classList.remove("showing");
+//   // nodeB.classList.add("hidden");  
+//        a.classList.replace("showing", "hidden");
+//        b.classList.replace("showing", "hidden");
+//        yesCount = 0;
+//        noCount = 0;
+// } else {
+//   yesCount = 0;
+//          noCount = 0;
+//   return;
+// }}, 500)
+// } else {
+//   console.log("ERROR")
+// }
+// }
+  }}
 
 // if the click is a first time click of two diff divs (console.log, ie. only 1 img has .showing && !.hidden), allow it
 // if the click is a second time click (console.log, 2 img   has .showing && !.hidden) 
 // grab the 2 images and store in an arr
 // check if both images are the same: colors.forEach((color) => {if (color == colorShowing[0] && color == colorShowing[1]) {console.log("Match") } 
 // else {"No Match"})} ); if images are not the same
+
+// refractor:
+// create an array of colors
+// before evaluating strA and strB delete the suffixes
+//
